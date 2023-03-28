b343903691a0817e98fa80db26d6bf482332c3d4
Roshan <roshankumar40786@gmail.com>
2023-03-28 13:55:46 +0530

    working on banner sections

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Image from '../spider.jpg'
 const Banner = () => {
-  return (
-    <div>
-      this is banner page
-    </div>
-  )
+    return (
+        <div className={`bg-[url(${Image})] h-[60vh] bg-no-repeat bg-cover bg-center flex items-end `}>
+            <div className='text-3xl text-white bg-gray-900 w-full text-center py-4 bg-opacity-50'>Spider-Man : No Way Home</div>
+        </div>
+    )
 }
 
 export default Banner
